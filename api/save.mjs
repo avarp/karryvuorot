be54ca15d3be770af4593bb79350201913d0f57e
Artem Vorobev <artem.v.mailbox@gmail.com>
2023-10-27 21:07:30 +0300

    1

--- api/save.mjs
+++ api/save.mjs
@@ -2,6 +2,9 @@ import { put } from "@vercel/blob";
 
 export default async function handler(request, response) {
   return response.send(
-    await put("shifts.json", '{"foo":"bar"}', { access: "public" })
+    await put("shifts.json", '{"foo":"bar"}', {
+      access: "public",
+      token: "vercel_blob_rw_NIKSdiQV7xsOojuX_8JZtZQYd3M6pEbcHUkda8wfW95VOk2",
+    })
   );
 }
