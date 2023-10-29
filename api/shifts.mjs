0ca1e3c019e39e586578988c8709ee364814c97e
Artem Vorobev <artem.v.mailbox@gmail.com>
2023-10-29 14:44:26 +0200

    1

--- api/shifts.mjs
+++ api/shifts.mjs
@@ -2,14 +2,16 @@ import {kv} from "@vercel/kv";
 
 export default async function handler(request, response) {
   let shifts = await kv.hgetall("shifts");
-  shifts.names = Array(18).map(() => Array(2).fill(""));
-  // for (let key in shifts) {
-  //   if (key.startsWith("names")) {
-  //     let name = shifts[key];
-  //     let [_, shiftNum, nameNum] = key.split(".");
-  //     shifts.names[shiftNum][nameNum] = name;
-  //     delete shifts[key];
-  //   }
-  // }
+  shifts.names = Array(18)
+    .fill()
+    .map(() => Array(2).fill(""));
+  for (let key in shifts) {
+    if (key.startsWith("names")) {
+      let name = shifts[key];
+      let [_, shiftNum, nameNum] = key.split(".");
+      shifts.names[shiftNum][nameNum] = name;
+      delete shifts[key];
+    }
+  }
   response.json(shifts);
 }
