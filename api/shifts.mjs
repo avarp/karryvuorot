99c87e75b3b88ec46aeb7099b67ad2cab3e39275
Artem Vorobev <artem.v.mailbox@gmail.com>
2023-10-29 14:49:59 +0200

    1

--- api/shifts.mjs
+++ api/shifts.mjs
@@ -9,6 +9,7 @@ export default async function handler(request, response) {
     if (key.startsWith("names")) {
       let name = shifts[key];
       let [_, shiftNum, nameNum] = key.split(".");
+      console.log(shiftNum, nameNum, key);
       shifts.names[shiftNum][nameNum] = name;
       delete shifts[key];
     }
